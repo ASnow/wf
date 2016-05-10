4015e237f10b85a17884e45491e3a70a14068ca4
Андрей Большов <bolshov@uniweb.ru>
2016-05-10 12:41:08 +0300

    add Gemfile.local install

--- lib/wf/install.rb
+++ lib/wf/install.rb
@@ -1,17 +1,41 @@
+require "fileutils"
+
 module Wf
   # installs wf
   class Install
-    def self.install
-      log 'Install for linux-amd64'
-      `mkdir tmp`
-      Dir.chdir('tmp') do
-        `wget https://github.com/github/hub/releases/download/v2.2.1/hub-linux-amd64-2.2.1.tar.gz`
-        `tar -xzf hub-linux-amd64-2.2.1.tar.gz`
-        `rm hub-linux-amd64-2.2.1.tar.gz`
-        Dir.chdir('hub-linux-amd64-2.2.1') do
-          log 'sudo cp hub /usr/local/bin/hub'
-          `sudo cp hub /usr/local/bin/hub`
+    class << self
+      include Wrapper::Cmd
+
+
+      def install
+        log 'Install bundler'
+        FileUtils.mkdir_p '.bundle'
+        File.open('.bundle/Gemfile.local', "w+") do |fp|
+          fp.write <<-GEMFILE
+# Include the regular Gemfile
+class Bundler::Dsl
+  def gemspec_with_feature(opts = nil)
+    opts ||= {}
+    opts[:path] ||= "../."
+
+    gemspec_without_feature opts
+  end
+  alias_method :gemspec_without_feature, :gemspec
+  alias_method :gemspec, :gemspec_with_feature
+end
+default_dir = File.expand_path('../..', __FILE__)
+Dir.chdir(default_dir)
+eval File.read('Gemfile')
+
+# Add any gems and groups that you don't want to keep local
+group :development do
+  gem 'wf', github: 'asnow/wf'
+end
+GEMFILE
         end
+        log "Run: bundle --gemfile '.bundle/Gemfile.local'"
+        result = run 'bundle', '--gemfile :file', with: {file: '.bundle/Gemfile.local'}, return: :bool
+        log "Result: #{result ? "OK" : "FIAL"}"
       end
     end
   end
